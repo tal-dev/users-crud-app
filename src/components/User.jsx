f8b69b3f88d327c2cc8c72e6183b5b237bb75cd3
tal-dev <talgat.abykeev@gmail.com>
2023-01-11 21:56:00 -0600

    finished post request

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,8 +1,10 @@
-const User = ({user}) => {
+const User = ({user, onDeleteUser}) => {
     return (
         <div className="card">
             <h3>Name: {user.name}</h3>
             <h3>Age: {user.age}</h3>
+            <button className="delete-btn" onClick={() => onDeleteUser(user.id)}>Delete</button>
+            <button className="delete-btn">Edit</button>
         </div>
     )
 }
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -6,17 +6,61 @@ const api = "https://63bc6b1efa38d30d85c77b01.mockapi.io/users"
 
 const Users = () => {
     const [users, setUsers] = useState([])
+    const [isFormOpen, setIsFormOpen] = useState(false)
 
     const fetchUsers = async () => {
-        const res = await axios.get(api)
-        setUsers(res.data)
+        try {
+            const res = await axios.get(api)
+            setUsers(res.data)
+        }
+        catch (e) {
+            console.log(e)
+        }
     }
 
     useEffect(() => {
         fetchUsers()
     }, [])
 
-    return users.map(user => <User user={user} />)
+    const deleteUser = async (id) => {
+        try {
+            const res = await axios.delete(api + "/" + id)
+            setUsers(users.filter(user => user.id !== id))
+        }
+        catch (e) {
+            if (e.response.status === 404) {
+                alert("Please try later")
+            }
+        }
+    }
+
+    const addUser = async (e) => {
+        e.preventDefault()
+        const newUser = {
+            name: e.target.elements.name.value,
+            age: e.target.elements.age.value
+        }
+        try {
+            const res = await axios.post(api, newUser)
+            setUsers([res.data].concat(users))
+            setIsFormOpen(false)
+        }
+        catch(e) {
+            console.log(e)
+        }
+    }
+
+    return (
+        <>
+            {!isFormOpen && <button onClick={() => setIsFormOpen(true)}>Create user</button>}
+            {isFormOpen && <form onSubmit={addUser}>
+                <input type="text" placeholder="name" name="name"/>
+                <input type="number" placeholder="age" name="age"/>
+                <button type="submit">Add</button>
+            </form>}
+            {users.map(ele => <User user={ele} onDeleteUser={deleteUser} />)}
+        </>
+    )
 }
 
 export default Users
